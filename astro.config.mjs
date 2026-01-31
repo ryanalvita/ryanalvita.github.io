787fbc4b71b345ba2f8c0a62f83e8d352fad49b0
Ryan <ryanalvita@yahoo.com>
2026-01-31 10:25:38 +0100

    build and deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://ryanalvita.github.io",
   vite: {
     plugins: [tailwindcss()],
   },
